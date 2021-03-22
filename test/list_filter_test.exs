36f4532fe86694e5e48865dd4d2a5bbaffc52ff1
Wilson Neto <wilsonpsn@hotmail.com>
2021-03-22 17:02:49 -0300

    It's working!!!!

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -4,5 +4,7 @@ defmodule ListFilterTest do
 
   test "filtering list for odd numbers" do
     assert ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"]) == 3
+    assert ListFilter.call(["3", "6", "43", "banana", "6", "abc", "melão"]) == 2
+    assert ListFilter.call(["abc", "comida"]) == 0
   end
 end
